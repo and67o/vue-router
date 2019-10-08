3516958e135ace403f101f3bfc83a04ee05055e6
oleg <kenny_67@mail.ru>
2019-10-08 21:11:20 +0300

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+
+import App from './../assets/js/views/App'
+import Hello from './../assets/js/views/Hello'
+import Home from './../assets/js/views/Home'
+import UsersIndex from './../assets/js/views/UsersIndex';
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component: Hello,
+        },
+        {
+            path: '/users',
+            name: 'users.index',
+            component: UsersIndex,
+        },
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/users', function () {
+    if (rand(1, 10) < 3) {
+        abort(500, 'We could not retrieve the users');
+    }
+    
+    return factory('App\User', 10)->make();
+});
