9cf668700339c3a5f3c4e9262c9421402a0e18a3
oleg <kenny_67@mail.ru>
2019-10-09 22:56:33 +0300

    add pagination

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/users', function () {
-    if (rand(1, 10) < 3) {
-        abort(500, 'We could not retrieve the users');
-    }
-    
-    return factory('App\User', 10)->make();
+Route::namespace('Api')->group(function () {
+    Route::get('/users', 'UsersController@index');
 });
